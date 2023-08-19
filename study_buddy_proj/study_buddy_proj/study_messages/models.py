48926e08bc73de8895dd5838ca2b05268156397d
Antonio0512 <antonioboyanov9@gmail.com>
2023-08-19 22:46:21 +0300

    Adding admins and models

--- study_buddy_proj/study_buddy_proj/study_messages/models.py
+++ study_buddy_proj/study_buddy_proj/study_messages/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from ..rooms.models import Room
 
-# Create your models here.
+User = get_user_model()
+
+
+class Messages(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    message = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message[0:50]
